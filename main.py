e2523f44e024487dcb0de85b6d73ae4687d8194a
Wojciech Mamys <woj.mam@gmail.com>
2023-03-13 22:57:23 +0100

    Changing structure of logos

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ box_size = 10
 border = 4
 fill_color = 'black'
 back_color = 'white'
-logo_link = 'logo.png'
+logo_link = 'logos/luczniczqa_logo.png'
 error_correction = qrcode.constants.ERROR_CORRECT_H
 
 # URLs and filenames:
-results_directory= 'results'
+results_directory = 'results'
 luczniczqa_website = 'https://www.luczniczqa.pl'
 luczniczqa_slack = 'https://join.slack.com/t/luczniczqa/shared_invite/zt-1r2pi3ckz-AmsPdWoFelLzPMscYRbf1A'
 luczniczqa_facebook = 'https://www.facebook.com/LuczniczQA'
