d8b24f990a042eb21f17311e793e913c233560b5
Wojciech Mamys <woj.mam@gmail.com>
2024-02-09 11:33:40 +0100

    Refactoring main method docstring

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from generator_utils import generate_qr_code_for_all_set
 from results_utils import properties
 
-
+"""
+Main method
+"""
 if __name__ == '__main__':
-    """Return the pathname of the KOS root directory."""
     generate_qr_code_for_all_set(configs=properties())
