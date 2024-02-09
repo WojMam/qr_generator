ea22f5c06f0eb5d19aeb8f21cd911635d2dbd066
Wojciech Mamys <woj.mam@gmail.com>
2024-02-09 12:23:45 +0100

    Fixing missing parameter in gereating all qr codes method

--- generator_utils.py
+++ generator_utils.py
@@ -26,7 +26,7 @@ def generate_qr_code_without_logo(configs, data_to_encode, filename_prefix):
                        box_size=configs.get("box_size").data,
                        border=configs.get("border").data)
     qr.add_data(data_to_encode)
-    img = qr.make_image(fill_color=configs.get("fill_color").data, 
+    img = qr.make_image(fill_color=configs.get("fill_color").data,
                         back_color=configs.get("back_color").data)
     img.save(f'./results/{filename_prefix}_qr_code_without_logo.png')
 
@@ -71,16 +71,16 @@ def generate_qr_code_with_logo(configs, data_to_encode, filename_prefix, logo_to
     create_results_directory(configs)
     logo = Image.open(logo_to_encode)
     basewidth = 100
-    wpercent = (basewidth / float(logo.size[0]))
+    wpercent =(basewidth / float(logo.size[0])
     hsize = int((float(logo.size[1]) * float(wpercent)))
     logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
-    QRcode = qrcode.QRCode(
+    qrcode_object = qrcode.QRCode(
         error_correction=qrcode.constants.ERROR_CORRECT_H
     )
-    QRcode.add_data(data_to_encode)
-    QRcode.make()
+    qrcode_object.add_data(data_to_encode)
+    qrcode_object.make()
     qr_color = configs.get("fill_color").data
-    qr_image = QRcode.make_image(
+    qr_image = qrcode_object.make_image(
         fill_color=qr_color, back_color=configs.get("back_color").data).convert('RGB')
     pos = ((qr_image.size[0] - logo.size[0]) // 2,
            (qr_image.size[1] - logo.size[1]) // 2)
@@ -98,24 +98,24 @@ def generate_qr_code_for_all_set(configs):
     filename_prefix (string): Prefix to the filename that will be saved as result
     """
 
-    generate_qr_code_without_logo(configs.get("luczniczqa_website"),
+    generate_qr_code_without_logo(configs, configs.get("luczniczqa_website"),
                                   'website')
-    generate_qr_code_with_logo(configs.get("luczniczqa_website"),
+    generate_qr_code_with_logo(configs, configs.get("luczniczqa_website"),
                                'website',
                                configs.get("logo_link"))
-    generate_qr_code_without_logo(configs.get("luczniczqa_slack"),
+    generate_qr_code_without_logo(configs, configs.get("luczniczqa_slack"),
                                   'slack')
-    generate_qr_code_with_logo(configs.get("luczniczqa_slack"),
+    generate_qr_code_with_logo(configs, configs.get("luczniczqa_slack"),
                                'slack',
                                configs.get("logo_link"))
-    generate_qr_code_without_logo(configs.get("luczniczqa_linkedin"),
+    generate_qr_code_without_logo(configs, configs.get("luczniczqa_linkedin"),
                                   'linkedin')
-    generate_qr_code_with_logo(configs.get("luczniczqa_linkedin"),
+    generate_qr_code_with_logo(configs, configs.get("luczniczqa_linkedin"),
                                'linkedin',
                                configs.get("logo_link"))
-    generate_qr_code_without_logo(configs.get("luczniczqa_facebook"),
+    generate_qr_code_without_logo(configs, configs.get("luczniczqa_facebook"),
                                   'facebook')
-    generate_qr_code_with_logo(configs.get("luczniczqa_facebook"),
+    generate_qr_code_with_logo(configs, configs.get("luczniczqa_facebook"),
                                'facebook',
                                configs.get("logo_link"))
     generate_qr_code_without_logo_svg(configs, configs.get("luczniczqa_website").data,
