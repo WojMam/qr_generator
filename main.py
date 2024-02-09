66b5abd643e968341b640450c814e1a9da0248bc
Wojciech Mamys <woj.mam@gmail.com>
2024-02-09 11:36:27 +0100

    Editing gitignore with additional output files formats

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from generator_utils import generate_qr_code_for_all_set
 from results_utils import properties
 
-"""
-Main method
-"""
+
 if __name__ == '__main__':
     generate_qr_code_for_all_set(configs=properties())
