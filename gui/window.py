dd71009ea02f3ebce72da28220bdb507fd2e838a
Wojciech Mamys <woj.mam@gmail.com>
2024-02-12 14:29:35 +0100

    Adding functionalities to buttons

--- gui/window.py
+++ gui/window.py
@@ -2,26 +2,48 @@
     view and it's various methods.
 """
 
+import os
+import platform
+import subprocess
 import tkinter as tk
 from tkinter import messagebox
 
-def show_message_box():
+from generator_utils import generate_qr_code_for_all_set
+from results_utils import properties
+
+def generate_all_qr_codes():
     """
     This method shows the popup window with welcoming message.
     """
-    messagebox.showinfo("Message", "Hello, this is a message box!")
+    generate_qr_code_for_all_set(configs=properties())
+    messagebox.showinfo("Message", "All QR codes were generated")
+
+def open_results_dir():
+    """
+    This method opens results directory in system explorer.
+    """
+    path="./results"
+    if platform.system() == "Windows":
+        os.startfile(path)
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", path])
+    else:
+        subprocess.Popen(["xdg-open", path])
 
 def initialize_window():
     """
     This method create the main app window.
     """
     root = tk.Tk()
-    root.title("Simple Window App")
+    root.title("QR generator")
 
     label = tk.Label(root, text="Click the button to show a message box")
     label.pack(pady=10)
 
-    button = tk.Button(root, text="Click Me!", command=show_message_box)
-    button.pack()
+    button_generate = tk.Button(root, text="Generate", command=generate_all_qr_codes)
+    button_generate.pack()
+
+    button_open_results_dir = tk.Button(root, text="Generate", command=open_results_dir)
+    button_open_results_dir.pack()
 
     root.mainloop()
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 """ This is a main project python module."""
 
-from generator_utils import generate_qr_code_for_all_set
 from gui.window import initialize_window
-from results_utils import properties
 
 
 if __name__ == '__main__':
-    generate_qr_code_for_all_set(configs=properties())
     initialize_window()
